15c763b3369691ae84774e326089da33b9c308ee
Federico Ruggi <f.ruggi@icloud.com>
2017-07-10 12:01:24 +0200

    updated \o\

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -5,6 +5,12 @@ info "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 success "Done"
 
+for tap in `cat brew/taps.list` ; do
+  info "Tapping formulas for $tap"
+  brew tap $tap
+  success "Done"
+done
+
 for package in `cat brew/install.list` ; do
   info "Installing $package"
   brew install $package
